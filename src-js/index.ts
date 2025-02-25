7eebb70ef3abda35b92bdcc4a212a24b14b43407
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-02-25 16:56:50 +0100

    style: Cleanup

--- src-js/index.ts
+++ src-js/index.ts
@@ -2,9 +2,6 @@ import { dirname, resolve } from 'path';
 import type { OpenWebpageOptions } from '../index';
 import { fileURLToPath } from 'url';
 
-// const _require = global.require ?? createRequire(import.meta.url);
-// const resolve = typeof require === 'function' ? require.resolve : import.meta.resolve;
-
 const _dirname = typeof __dirname == 'string' ? __dirname : dirname(fileURLToPath(import.meta.url));
 
 export async function openWebpage(options: OpenWebpageOptions = {}, execaOptions = {}) {
