f04209c44820337c7462f6af5ea89addaf4e9b5c
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2026-04-18 09:23:46 +0200

    style: Support all options

--- src-js/bin/index.ts
+++ src-js/bin/index.ts
@@ -4,12 +4,24 @@ import { program } from 'commander';
 import { version, name } from '../../package.json';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { kebabCase } from 'change-case';
+import { properties } from '../__generated__/options.schema.json';
+
+program.name(`npx ${name}`).version(version);
+
+for (const [_name, { description }] of Object.entries(properties)) {
+  const name = kebabCase(_name);
+
+  // Use url as an argument
+  if (name === 'url') {
+    program.argument(`[${name}]`, description);
+  } else {
+    program.option(`--${name}`, description);
+  }
+}
 
 program
-  .name(`npx ${name}`)
-  .version(version)
-  .argument('<url>', 'The URL to open')
-  .action(async (url, opts) => {
+  .action(async function run(url, opts) {
     const _dirname =
       typeof __dirname == 'string' ? __dirname : dirname(fileURLToPath(import.meta.url));
     const syncPath = resolve(_dirname, '../sync/open-webpage.mjs');
@@ -17,6 +29,5 @@ program
     const { execaNode } = await import('execa');
 
     await execaNode({})`${syncPath} ${JSON.stringify({ url, ...opts })}`;
-  });
-
-program.parseAsync();
+  })
+  .parseAsync();
