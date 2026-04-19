f38ee640ac76b30e3206f911770c70a4c9ba840c
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2026-04-19 15:38:59 +0200

    style: Improve types

--- src-js/index.ts
+++ src-js/index.ts
@@ -1,10 +1,15 @@
 import { dirname, resolve } from 'path';
 import type { OpenWebpageOptions } from '../index';
 import { fileURLToPath } from 'url';
+import type { Options as ExecaOptions } from 'execa';
 
 const _dirname = typeof __dirname == 'string' ? __dirname : dirname(fileURLToPath(import.meta.url));
 
-export async function openWebpage(options: OpenWebpageOptions = {}, execaOptions = {}) {
+export async function openWebpage(
+  options: OpenWebpageOptions = {},
+  /** Options for execa (optional) @see https://www.npmjs.com/package/execa */
+  execaOptions: ExecaOptions = {},
+) {
   const { execaNode } = await import('execa');
 
   return execaNode(
